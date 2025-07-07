55fc40457eda6c95098819f67057ea4127f419bf
최한준(Choe, Hanjoon) <hanjoon.choe@bespinglobal.com>
2025-07-07 16:08:28 +0900

    fix

--- v1/retriever-simple-benchmark/module/reranker/transformers_reranker.py
+++ v1/retriever-simple-benchmark/module/reranker/transformers_reranker.py
@@ -1,13 +1,13 @@
 from .base import BaseReranker
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
-#from transformers import Qwen3ForCausalLM, Qwen3Tokenizer
+from transformers import Qwen3ForCausalLM
 
 class Qwen3Reranker(BaseReranker):
     def __init__(self, model_path: str):
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         print(f"Using device: {device}")
-        self.reranker = AutoModelForCausalLM.from_pretrained(model_path).to(device).eval()
+        self.reranker = Qwen3ForCausalLM.from_pretrained(model_path).to(device).eval()
         self.tokenizer = AutoTokenizer.from_pretrained(model_path, padding_side='left')
         self.token_false_id = self.tokenizer.convert_tokens_to_ids("no")
         self.token_true_id = self.tokenizer.convert_tokens_to_ids("yes")
